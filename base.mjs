09fa22ac09b47038798b718f98168d174705951d
Indy Van Canegem <i.vancanegem@faktion.com>
2025-06-25 17:21:30 +0200

    feat: setup

---
+++ base.mjs
@@ -0,0 +1,15 @@
+export default {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  singleAttributePerLine: true,
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
