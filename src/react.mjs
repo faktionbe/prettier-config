d972b94dd1141e9e2503b322939cd6e007ede503
Pieter-Jan De Bruyne <p.debruyne@faktion.com>
2025-08-12 17:28:55 +0200

    add tailwind related configs

--- src/react.mjs
+++ src/react.mjs
@@ -3,5 +3,9 @@ import base from "./base.mjs";
 export default {
   ...base,
   jsxSingleQuote: true,
+  tailwindPreserveWhitespace: true,
+  tailwindFunctions: [
+    "tw"
+  ],
   plugins: ["prettier-plugin-tailwindcss"],
 };
